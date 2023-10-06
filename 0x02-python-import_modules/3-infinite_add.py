c0956e29673e6cfec7166a21c74c8393809bd23f
Pacific1990 <abet.eko@gmail.com>
2023-10-05 20:03:01 -0700

    commit 004

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
-kkZZ
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    ac = len(argv) - 1
+
+    sum = 0
+    for i in range(1, ac + 1):
+        sum += int(argv[i])
+
+    print("{:d}".format(sum))Z
